c6b211c93000aeb1e7d948291c6fe8c7feac9e45
Rahul Anand <rahulanand2006@gmail.com>
2025-07-18 18:34:48 -0400

    Initial  Commit witht he UV Lock

---
+++ foundation/first.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+def loadAPIKeys():
+    load_dotenv(override=True)
+
+    openai_api_key = os.getenv('OPENAI_API_KEY')
+
+    if openai_api_key:
+        print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+    else:
+        print("OpenAI API Key not set - please head to the troubleshooting guide in the setup folder")
+
+if __name__ == "__main__" :
+     loadAPIKeys()
